609ed0b7c5a6196e0a25c22bc32457e9dae8cceb
Adam Nordquist <apnordquist@gmail.com>
2024-03-04 14:48:44 -0700

    initialization
    setup BasicMath() and Main()

---
+++ BasicMath.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    public class BasicMath
+    {
+        public double Addition(double x, double y)
+        { return x + y; }
+        public double Subtraction(double x, double y)
+        { return x - y; }
+        public double Multiplication(double x, double y)
+        { return x * y; }
+        public double Division(double x, double y)
+        {
+            if (y == 0)
+            { return 0; }
+            else
+            { return x / y; }
+        }
+    }
+}
